06092d90eb6537bb3625e131e8ab596371aa59e7
Nestoter <netcheverry@bps.gub.uy>
2022-08-09 13:52:09 -0300

    Subo primer versión.

---
+++ DisenaTuCursoDocente/ElectronEntry.js
@@ -0,0 +1,44 @@
+const {
+    app,
+    BrowserWindow
+    } = require('electron')
+  const url = require("url");
+  const path = require("path");
+
+  let appWindow
+
+  function initWindow() {
+    appWindow = new BrowserWindow({
+      width: 1000,
+      height: 800,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+
+    appWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, `/dist/disena-tu-curso-docente/index.html`),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+
+    appWindow.on('closed', function () {
+      appWindow = null
+    })
+  }
+
+  app.on('ready', initWindow)
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', function () {
+    if (win === null) {
+      initWindow()
+    }
+  })
