1dbed6643539eabb876139e8ad62826be6db3a92
Nestoter <netcheverry@bps.gub.uy>
2022-09-27 20:05:53 -0300

    Subo dependencias del Backend.

--- CompilarDisenaTuCursoDocente.ps1
+++ CompilarDisenaTuCursoDocente.ps1
@@ -1,4 +1,5 @@
-
+#https://stackoverflow.com/questions/19728555/node-js-require-doesnt-work-in-script
+#https://stackoverflow.com/questions/35682131/electron-packager-cannot-find-module
 $dirCompiladoAngular = "$PSScriptRoot\DisenaTuCursoDocente\out\disena-tu-curso-docente-win32-x64\resources\app\"
 $imgOrigen="$PSScriptRoot\DisenaTuCursoDocente\out\disena-tu-curso-docente-win32-x64\resources\app\dist\disena-tu-curso-docente\assets\img"
 $imgDestino="$PSScriptRoot\DisenaTuCursoDocente\out\disena-tu-curso-docente-win32-x64\resources\app\dist\assets"
@@ -12,7 +13,8 @@ write-host "COMPILANDO ELECTRON"
 npm run make
 
 write-host "ELIMINANDO ARCHIVOS INNECESARIOS"
-Get-ChildItem "$dirCompiladoAngular" -Exclude dist,ElectronEntry.js,Backend.js,package.json,loading.html | Remove-Item -Recurse -Force
+Get-ChildItem "$dirCompiladoAngular" -Exclude dist,ElectronEntry.js,Backend.js,package.json,loading.html,node_modules | Remove-Item -Recurse -Force
+Get-ChildItem "$dirCompiladoAngular\node_modules" -Exclude accepts,body-parser,call-bind,content-disposition,content-type,cookie,cookie-signature,cors,depd,destroy,ee-first,encodeurl,escape-html,etag,express,finalhandler,forwarded,fresh,function-bind,get-intrinsic,has,has-symbols,http-errors,iconv-lite,inherits,media-typer,merge-descriptors,methods,mime,mime-db,mime-types,negotiator,object-assign,object-inspect,on-finished,parseurl,path-to-regexp,proxy-addr,qs,range-parser,raw-body,safer-buffer,send,serve-static,setprototypeof,side-channel,statuses,toidentifier,type-is,unpipe,utils-merge,vary | Remove-Item -Recurse -Force
 
 write-host "MOVIENDO CARPETA IMG"
 if(!(Test-Path -Path "$imgDestino")){
