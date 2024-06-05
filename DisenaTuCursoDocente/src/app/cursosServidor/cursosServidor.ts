e85f3c18ee2624b3666d6d70efa82d1bc1e0e29d
Sebastian Pelaez <pelaez.seba@gmail.com>
2024-06-04 23:36:50 -0300

    bajar curso del server
    listar curso y bajar curso

--- DisenaTuCursoDocente/src/app/cursosServidor/cursosServidor.ts
+++ DisenaTuCursoDocente/src/app/cursosServidor/cursosServidor.ts
@@ -1,116 +1,135 @@
 import { Component, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-imports: [
-  NgbModule
-]
-import { __values } from 'tslib';
-import { GrupoDatoFijo } from '../modelos/schema.model';
 import { InitialSchemaLoaderService } from '../servicios/initial-schema-loader.service';
+import { SchemaSavedData } from '../modelos/schemaData.model';
 
 @Component({
-    selector: 'app-cursosServidor',
-    templateUrl: './cursosServidor.html',
-    styleUrls: ['./cursosServidor.css'],
+  selector: 'app-cursosServidor',
+  templateUrl: './cursosServidor.html',
+  styleUrls: ['./cursosServidor.css'],
 })
 export class cursosServidorComponent {
-    title = 'DisenaTuCursoDocente';
-    token:string='';
-    servidor:string='';
-    cursosBuscados: any;
+  title = 'DisenaTuCursoDocente';
+  token: string = '';
+  servidor: string = '';
+  cursosBuscados: any[] = [];
+  termino: string = '';
 
-    constructor(private modalService: NgbModal, private router: Router,
-      public initialSchemaService : InitialSchemaLoaderService,
-      private route: ActivatedRoute) {}
+  constructor(private modalService: NgbModal, private router: Router,
+              public initialSchemaService: InitialSchemaLoaderService,
+              private route: ActivatedRoute) {}
 
-    ngOnInit(): void {
-      // Recuperar los parámetros de la URL
-      this.route.queryParams.subscribe(params => {
-        this.token = params['token'];
-        this.servidor = params['servidor'];
-        // Hacer lo que necesites con el token y la URL del servidor
-        console.log('Token:', this.token);
-        console.log('URL del servidor:', this.servidor);
-        // Llamar a la función que necesites pasándoles estos valores
-      });
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+      this.servidor = params['servidor'];
+      console.log('Token:', this.token);
+      console.log('URL del servidor:', this.servidor);
+    });
 
-      const alert = document.querySelector('ngb-alert')
-      if(alert)
-        alert.classList.remove('show')
-    }
-
-
-
-    cardClick(idCurso: any) {
-      //cuardo el curso en la pc local
-    }
-
-    goHome(){
-      this.router.navigate(['/']);
+    const alert = document.querySelector('ngb-alert')
+    if(alert)
+      alert.classList.remove('show');
   }
 
+  onSubmit(event: Event) {
+    event.preventDefault();
+    this.buscarCurso(this.termino);
+  }
 
-    async listarCursos() {
-      let headers = new Headers();
-      headers.append('Accept', 'application/json');
+  async buscarCurso(termino: string) {
+    console.log('Buscando curso con término:', termino);
+    const apiUrl = `${this.servidor}/api/listarCursos?criterio=${encodeURIComponent(termino)}`;
 
-      try {
-        const response = await fetch('http://localhost:'+this.initialSchemaService.puertoBackend+'/cursos', {
-          method: 'GET',
-          headers: headers,
-          mode: 'cors',
-        });
-        const cursos = await response.json();
-        if (response.status === 200)
-          console.log('Cursos obtenidos exitosamente', cursos);
-        else console.log('Ha ocurrido un error, ', response.status);
-      } catch (e) {
-        const alert = document.querySelector('ngb-alert')
-        if(alert)
-          alert.classList.add('show')
-        console.error(e);
+    console.log(apiUrl);
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.token}`,
+        }
+      });
+
+      if (response.ok) {
+        this.cursosBuscados = await response.json();
+        console.log(this.cursosBuscados);
+      } else {
+        console.log('Ha ocurrido un error:', response.status);
+        alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
       }
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+      alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
     }
+  }
 
-      @HostListener("click", ["$event"])
-      public onClick(event: any): void
-      {
-          event.stopPropagation();
-          var target = event.target;
-          if (!target.closest(".dropdown-menu") && !target.closest(".dropdown-toggle")) {
-              // do whatever you want here
-              let dropdown = document.querySelectorAll(".dropdown-menu")
-              dropdown.forEach(div => {
-                  if (div.classList.contains("show"))
-                      div.classList.remove('show');
+  async cardClick(idCurso: any) {
+    console.log('Buscando curso:', idCurso);
+    const apiUrl = `${this.servidor}/api/bajarCurso?idCurso=${encodeURIComponent(idCurso)}`;
 
-              });
-          }
+    console.log(apiUrl);
 
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.token}`,
+        }
+      });
+
+      if (response.ok) {
+        const cursoB64 = await response.json();
+        const cursoJson = JSON.parse(atob(cursoB64.base64));
+        console.log(cursoJson);
+        this.descargarCurso(cursoJson);
+
+      } else {
+        console.log('Ha ocurrido un error:', response.status);
+        alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
       }
-
-
-    muestroHeader(){
-        let vuelta = this.initialSchemaService.loadedData !== undefined;
-        console.log("muestroHeader: "+vuelta);
-        return vuelta;
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+      alert('Error en la búsqueda. Intente luego o consulte al administrador del sistema.');
     }
 
-    muestroMisCursos(){
-        let vuelta = this.initialSchemaService.allData === undefined && this.initialSchemaService.loadedData !== undefined;
-        console.log("muestroMisCursos: "+vuelta);
-        return vuelta;
-    }
+  }
 
-    muestroCursosCompartidosConmigo(){
-        let vuelta = this.initialSchemaService.allData && !this.initialSchemaService.loadedData;
-        console.log("muestroCursosCompartidosConmigo: "+vuelta);
-        return vuelta;
-    }
+  goHome() {
+    this.router.navigate(['/']);
+  }
 
-    muestroNoExistenCursos(){
-        let vuelta = !this.initialSchemaService.allData;
-        console.log("muestroNoExistenCursos: "+vuelta);
-        return vuelta;
+  async descargarCurso(curso: SchemaSavedData) {
+    curso.id = (this.initialSchemaService.allData?.length || 0) + 1;
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    try {
+      const response = await fetch(`http://localhost:${this.initialSchemaService.puertoBackend}/cursos/${curso?.id}`, {
+        method: 'PUT',
+        headers: headers,
+        mode: 'cors',
+        body: JSON.stringify({
+          curso: { ...curso, fechaModificacion: new Date() },
+        }),
+      });
+      if (response.status === 200){
+        console.log('Curso descargado exitosamente');
+        alert("Curso descargado exitosamente. Podrás verlo en el Inicio.")
+        this.goHome();
+      }
+      else {
+        console.log('Ha ocurrido un error, ', response.status);
+        alert('Error al modificar el curso. Intente nuevamente.');
+      }
+    } catch (e) {
+      const alert = document.querySelector('ngb-alert');
+      if (alert)
+        alert.classList.add('show');
+      console.error(e);
     }
+  }
 }
